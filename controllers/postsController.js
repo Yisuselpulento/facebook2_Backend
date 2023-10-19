f40d546c55fcde520b9d4114a37f7a713d695064
MonssterCore <monssteremottion@gmail.com>
2023-10-19 02:41:51 -0300

    likes terminados

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,26 +1,24 @@
 import Post from "../models/Posts.js"
 
-
-const getGlobalPost = async (req , res) => {
+const getGlobalPost = async (req, res) => {
     try {
         const posts = await Post.find()
-                                .populate('author', 'image nombre')
-                                .populate({ 
-                                    path: 'comments', 
-                                    populate: { 
-                                        path: 'author', 
-                                        select: 'nombre' 
-                                    } 
-                                })
-                                .sort({ createdAt: -1 })
-                                .exec();
-
-        res.json(posts)
+            .populate('author', 'image nombre')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'author',
+                    select: 'nombre'
+                }
+            })
+            .sort({ createdAt: -1 })
+            .exec();
+        
+        res.json(posts);
     } catch (error) {
-        res.status(500).json({ message: "Error al obtener los posts", error: error.message });
+        res.status(500).json({ message: "Error al obtener los posts" });
     }
 }
-
 
 const newPost = async (req, res) => {
     const newPost = new Post({
@@ -33,44 +31,39 @@ const newPost = async (req, res) => {
 
     try {
         await newPost.save();
-
-        // Populamos el post guardado con la información detallada del autor.
         const populatedPost = await Post.findById(newPost._id)
-                                        .populate('author', 'image nombre')
-                                        .populate({
-                                            path: 'comments',
-                                            populate: {
-                                                path: 'author',
-                                                select: 'nombre'
-                                            }
-                                        })
-                                        .exec();
-
+            .populate('author', 'image nombre')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'author',
+                    select: 'nombre'
+                }
+            })
+            .exec();
+        
         res.json(populatedPost);
     } catch (error) {
-        console.log("error al crear post", error);
-        res.status(500).json({ message: "Error al crear el post", error: error.message });
+        res.status(500).json({ message: "Error al crear el post" });
     }
 }
 
-
-const deletePost = async (req , res) => {
-
+const deletePost = async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        
-         if (!post) {
+
+        if (!post) {
             return res.status(404).json({ error: 'Post no encontrado' });
         }
-        
+
         if (post.author.toString() !== req.usuario._id.toString()) {
             return res.status(403).json({ error: 'No tienes permiso para eliminar este post' });
         }
-        
+
         await post.deleteOne();
-        res.json({ success: 'Post eliminado con éxito' });   
+        res.json({ success: 'Post eliminado con éxito' });
     } catch (error) {
-        res.status(500).json({ error: 'Hubo un error al eliminar el post' })
+        res.status(500).json({ error: 'Hubo un error al eliminar el post' });
     }
 }
 
@@ -80,45 +73,40 @@ const giveLike = async (req, res) => {
         const userId = req.usuario._id;
         const userName = req.usuario.nombre;
         const userImage = req.usuario.image;
-        const post = await Post.findById(postId);
 
         if (!userId) {
-            return res.status(404).send({ message: "Usuario no encontrado." });
+            return res.status(404).json({ message: "Usuario no encontrado." });
         }
 
+        const post = await Post.findById(postId);
         if (!post) {
-            return res.status(404).send({ message: "Post no encontrado." });
+            return res.status(404).json({ message: "Post no encontrado." });
         }
 
-        if (!post.likes) {
-            post.likes = [];
-        }
+        const userAlreadyLiked = post.likes.some(like => like.userId.equals(userId));
 
-        const likeIndex = post.likes.findIndex(like => like.userId.equals(userId));
-
-        let hasLiked;
-        if (likeIndex !== -1) {
-            post.likes.splice(likeIndex, 1);
-            hasLiked = false;
+        if (userAlreadyLiked) {
+            await Post.findByIdAndUpdate(postId, {
+                $pull: { likes: { userId } }
+            });
         } else {
-            post.likes.push({ userId, userName, userImage });
-            hasLiked = true;
+            await Post.findByIdAndUpdate(postId, {
+                $push: { likes: { userId, userName, userImage } }
+            });
         }
 
-        await post.save();
+        // Recuperar el post después de la actualización para obtener la lista actualizada de "likes"
+        const updatedPost = await Post.findById(postId);
 
-        res.status(200).send({
-            likesCount: post.likes.length,
-            hasLiked,
-            user: {
-                userId,
-                userName,
-                userImage
-            },
-            likesUsers: post.likes
+        res.json({
+            likesCount: updatedPost.likes.length,
+            hasLiked: !userAlreadyLiked,
+            user: { userId, userName, userImage },
+            likesUsers: updatedPost.likes
         });
+
     } catch (error) {
-        res.status(500).send({ message: "Error al procesar la solicitud.", error: error.message });
+        res.status(500).json({ message: "Error al procesar la solicitud." });
     }
 };
 
